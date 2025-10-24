d41fd4f0497dcbaf37620dffa2b4e14b7b6d793f
ShubhamTheDataGuy <shubhamnagpal789@gmail.com>
2025-10-25 00:59:58 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Shubham Nagpal",
+    author_email="shubhamnagpal789@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
